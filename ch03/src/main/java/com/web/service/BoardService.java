f5a8b2a1f1fe7246d90eb8d80093bfcf0172a89f
Shin seongsu <tlstjdtn321@naver.com>
2020-05-07 23:52:56 +0900

    Service 단

---
+++ ch03/src/main/java/com/web/service/BoardService.java
@@ -0,0 +1,15 @@
+package com.web.service;
+
+import com.web.repository.BoardRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class BoardService {
+
+    private final BoardRepository boardRepository;
+
+    public BoardService(BoardRepository boardRepository) {
+        this.boardRepository = boardRepository;
+    }
+
+}
