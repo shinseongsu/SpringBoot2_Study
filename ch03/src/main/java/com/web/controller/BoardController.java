2fa3db6e22546faae5ae998e12fc9eb77f5b3618
Shin seongsu <tlstjdtn321@naver.com>
2020-05-08 00:03:05 +0900

    컨트롤러 다시올리기

--- ch03/src/main/java/com/web/controller/BoardController.java
+++ ch03/src/main/java/com/web/controller/BoardController.java
@@ -2,10 +2,13 @@ package com.web.controller;
 
 import com.web.service.BoardService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/board")
@@ -15,8 +18,16 @@ public class BoardController {
     BoardService boardService;
 
     @GetMapping({"", "/"})
-    public String board(@RequestMapping(value = "idx", defaultValue = "0") Long idx, Model model) {
+    public String board(@RequestParam(value = "idx", defaultValue = "0") Long idx, Model model) {
+        model.addAttribute("board", boardService.findBoardByIdx(idx));
+        return "/board/form";
+    }
 
+    @GetMapping("/list")
+    public String list(@PageableDefault Pageable pageable, Model model) {
+        model.addAttribute("boardList", boardService.findBoardList(pageable));
+
+        return "/board/list";
     }
 
 }
