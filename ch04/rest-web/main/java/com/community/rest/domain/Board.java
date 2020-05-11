e4abbb3beaa77b08ec5d726359e32a434e19fbdf
Shin seongsu <tlstjdtn321@naver.com>
2020-05-11 23:08:31 +0900

    nulti

---
+++ ch04/rest-web/main/java/com/community/rest/domain/Board.java
@@ -0,0 +1,14 @@
+package main.java.com.community.rest.domain;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+@Getter
+@NoArgsConstructor
+@Entity
+public class Board implements Serializable {
+
+
+}
