f0edd2592ce09ed4e2017c6a8b828503a65c4dc5
Shin seongsu <tlstjdtn321@naver.com>
2020-05-19 22:35:44 +0900

    Entity 설정

--- ch07/src/main/java/com/example/spring/vo/Ingredient.java
+++ ch07/src/main/java/com/example/spring/vo/Ingredient.java
@@ -1,10 +1,16 @@
 package com.example.spring.vo;
 
+import lombok.AccessLevel;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
+
+import javax.persistence.Entity;
 
 @Data
 @RequiredArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PRIVATE, force = true)
+@Entity
 public class Ingredient {
 
     private final String id;
