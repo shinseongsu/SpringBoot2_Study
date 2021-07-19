93eca8d565941418385972ad94fabce0f1f40fa6
Shin seongsu <tlstjdtn321@naver.com>
2021-07-19 22:33:42 +0900

    visual Code로 스프링부트 만들어보기

---
+++ VSSpringBoot/JavaHelloWorld.java
@@ -0,0 +1,8 @@
+/**
+ * JavaHelloWorld
+ */
+public class JavaHelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ VSSpringBoot/SpringBootHelloWorld/springboothelloworld/src/main/java/com/javacampus/springboothelloworld/HomeController.java
@@ -0,0 +1,19 @@
+package com.javacampus.springboothelloworld;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * HomeController
+ */
+@RestController
+public class HomeController {
+
+    @RequestMapping(value = "/home", method = RequestMethod.GET)
+    public String index() {
+        return "Hello World with Spring Boot";
+    }
+
+}
