b047ce41721c639467ddfd8914fe5672e751a751
Shin seongsu <tlstjdtn321@naver.com>
2021-07-20 23:32:16 +0900

    visualStudio로 공부하기

--- VSSpringBoot/SpringBootHelloWorld/springboothelloworld/src/main/java/com/javacampus/springboothelloworld/HomeController.java
+++ VSSpringBoot/SpringBootHelloWorld/springboothelloworld/src/main/java/com/javacampus/springboothelloworld/HomeController.java
@@ -1,6 +1,5 @@
 package com.javacampus.springboothelloworld;
 
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
